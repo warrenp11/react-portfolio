2e1bad8216b52d7c4ecd4ac811a6b5f368f10ced
Peter <warrenp11@gmail.com>
2022-01-29 14:05:01 -0500

    set up about component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <About></About>
     </div>
   );
 }
