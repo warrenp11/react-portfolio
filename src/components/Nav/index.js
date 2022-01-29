7589ef9954f87d6d3646089a7f8a69f79b38e79f
Peter <warrenp11@gmail.com>
2022-01-29 14:24:17 -0500

    map over nav categories

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 function Nav() {
+  const categories = [
+    { name: "portfolio", description: "Links to projects I've created as a web developer" },
+    { name: "resume", description: "My professional resume" },
+  ];
+
+  function categorySelected(name) {
+    console.log(`${name} clicked`)
+  }
+
   return (
     <header>
       <h2>
-        <a href="/">
-          My React Portfolio
-        </a>
+        <a href="/">My React Portfolio</a>
       </h2>
       <nav>
         <ul className="flex-row">
@@ -16,6 +23,11 @@ function Nav() {
           <li>
             <span>Contact</span>
           </li>
+          {categories.map((category) => (
+            <li className="mx-1" key={category.name}>
+              <span onClick={() => categorySelected(category.name)} >{category.name}</span>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
